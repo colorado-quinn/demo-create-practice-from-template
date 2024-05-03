6c1ca9fc0bcdf8f15f689f89b4198230734b2f5e
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-03 09:18:42 -0600

    add function to determine if template fits in practice time

--- app.js
+++ app.js
@@ -1,3 +1,77 @@
+const funTemplate = {
+  name: "Fun Template",
+  drills: [
+    {
+      name: "Warmup",
+      minDur: 5,
+      maxDur: 10,
+    },
+    {
+      name: "Dribbling",
+      minDur: 10,
+      maxDur: 20,
+    },
+    {
+      name: "Follow the Leader",
+      minDur: 15,
+      maxDur: 30,
+    },
+  ],
+};
+
+const intermediaryTemplate = {
+  name: "Intermediary Template",
+  drills: [
+    {
+      name: "Warmup",
+      minDur: 5,
+      maxDur: 10,
+    },
+    {
+      name: "Dribbling",
+      minDur: 15,
+      maxDur: 20,
+    },
+    {
+      name: "Three Cone Passing",
+      minDur: 20,
+      maxDur: 30,
+    },
+    {
+      name: "Four-Corner Shielding",
+      minDur: 20,
+      maxDur: 30,
+    },
+  ],
+};
+
+const calculateSum = (arr) => {
+  return arr.reduce((total, current) => {
+    return total + current;
+  }, 0);
+};
+
+const doesTemplateFitInPractice = (template, practiceDuration) => {
+  const minDur = calculateSum(template.drills.map((t) => t.minDur));
+  const maxDur = calculateSum(template.drills.map((t) => t.maxDur));
+  console.log(`${template.name} can run for ${minDur} - ${maxDur} mins.`);
+  const doesFit = minDur <= practiceDuration && practiceDuration <= maxDur;
+  console.log(
+    `${template.name} ${
+      doesFit ? "DOES" : "DOES NOT"
+    } fit into practice duration of ${practiceDuration} mins. \r\n`
+  );
+
+  return doesFit;
+};
+
+doesTemplateFitInPractice(funTemplate, 29);
+doesTemplateFitInPractice(funTemplate, 30);
+doesTemplateFitInPractice(funTemplate, 60);
+doesTemplateFitInPractice(funTemplate, 61);
 
 
-console.log('working');
+doesTemplateFitInPractice(intermediaryTemplate, 59);
+doesTemplateFitInPractice(intermediaryTemplate, 60);
+doesTemplateFitInPractice(intermediaryTemplate, 90);
+doesTemplateFitInPractice(intermediaryTemplate, 91);

c0bf172b2bb6476f0c77a3fb7b672135c256b58f
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-03 09:21:10 -0600

    refactor template data

---
+++ data.js
@@ -0,0 +1,51 @@
+const funTemplate = {
+  name: "Fun Template",
+  drills: [
+    {
+      name: "Warmup",
+      minDur: 5,
+      maxDur: 10,
+    },
+    {
+      name: "Dribbling",
+      minDur: 10,
+      maxDur: 20,
+    },
+    {
+      name: "Follow the Leader",
+      minDur: 15,
+      maxDur: 30,
+    },
+  ],
+};
+
+const intermediaryTemplate = {
+  name: "Intermediary Template",
+  drills: [
+    {
+      name: "Warmup",
+      minDur: 5,
+      maxDur: 10,
+    },
+    {
+      name: "Dribbling",
+      minDur: 15,
+      maxDur: 20,
+    },
+    {
+      name: "Three Cone Passing",
+      minDur: 20,
+      maxDur: 30,
+    },
+    {
+      name: "Four-Corner Shielding",
+      minDur: 20,
+      maxDur: 30,
+    },
+  ],
+};
+
+module.exports = {
+  funTemplate,
+  intermediaryTemplate,
+};
